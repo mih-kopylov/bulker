8f1f7939be2242b75c05c332921a117dadc21fd0
Mikhail Kopylov <mih.kopylov@yandex.ru>
2022-06-26 16:50:55 +0300

    #8 Implement repos filtering by name and by tag
    Apply filtering for `repo list` command
    
    Closes #8

--- cmd/repo/list.go
+++ cmd/repo/list.go
@@ -1,9 +1,10 @@
 package repo
 
 import (
-	"github.com/mih-kopylov/bulker/internal/settings"
+	"context"
+	"github.com/mih-kopylov/bulker/internal/config"
+	"github.com/mih-kopylov/bulker/internal/runner"
 	"github.com/mih-kopylov/bulker/internal/utils"
-	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
@@ -11,17 +12,27 @@ var ListCmd = &cobra.Command{
 	Use:   "list",
 	Short: "Prints a list of supported repositories",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		settingsManager := settings.NewManager(utils.GetConfiguredFS())
+		newRunner := runner.NewRunner(utils.GetConfiguredFS(), config.ReadConfig(), listFilter)
 
-		sets, err := settingsManager.Read()
+		err := newRunner.Run(
+			func(ctx context.Context, runContext *runner.RunContext) (runner.Result, error) {
+				return listResult{Message: runContext.Repo.Name}, nil
+			},
+		)
 		if err != nil {
 			return err
 		}
 
-		for _, repo := range sets.Repos {
-			logrus.Info(repo.Name)
-		}
-
 		return nil
 	},
+}
+
+type listResult struct {
+	Message string
+}
+
+var listFilter = &runner.Filter{}
+
+func init() {
+	listFilter.AddCommandFlags(ListCmd)
 }
