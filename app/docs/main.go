bfda4b4422f6bfe7539912998d17ccd837f55d35
Mikhail Kopylov <mih.kopylov@yandex.ru>
2022-07-26 21:51:26 +0300

    #41 Generate documentation in GH Wiki

---
+++ app/docs/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"github.com/mih-kopylov/bulker/cmd"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra/doc"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	rootCommand := cmd.CreateRootCommand()
+	rootCommand.DisableAutoGenTag = true
+	dir, err := filepath.Abs("./dist/docs")
+	if err != nil {
+		logrus.Fatal(err)
+	}
+
+	err = os.MkdirAll(dir, 0777)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+
+	err = doc.GenMarkdownTree(rootCommand, dir)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+}
