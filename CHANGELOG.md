f27d4d6b609acb985b8479055c9bc19a37acf213
Mikhail Kopylov <mih.kopylov@yandex.ru>
2022-08-06 07:41:14 +0300

    #80 Implement `git push` command

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Added
+
+- `git push` command
+
 ### Fixed
 
 - Empty array to be printed in `--output json` mode when no repositories matched
--- cmd/git/push.go
+++ cmd/git/push.go
@@ -7,25 +7,33 @@ import (
 	"github.com/spf13/cobra"
 )
 
-func CreatePullCommand() *cobra.Command {
+func CreatePushCommand() *cobra.Command {
 	var filter = runner.Filter{}
 
+	var flags struct {
+		branch string
+	}
+
 	var result = &cobra.Command{
-		Use:   "pull",
-		Short: "Pull changes from remote",
+		Use:   "push",
+		Short: "Push branches to remote",
+		Long: `Push branches to remote.
+If -b <branchName> is defined, pushes the only branch. Otherwise pushes all branches`,
 		RunE: runner.NewDefaultRunner(
 			&filter, func(ctx context.Context, runContext *runner.RunContext) (interface{}, error) {
-				err := gitops.Pull(runContext.Fs, runContext.Repo)
+				err := gitops.Push(runContext.Fs, runContext.Repo, flags.branch)
 				if err != nil {
 					return nil, err
 				}
 
-				return "pulled successfully", nil
+				return "pushed successfully", nil
 			},
 		),
 	}
 
 	filter.AddCommandFlags(result)
+
+	result.Flags().StringVarP(&flags.branch, "branch", "b", "", "Name of the branch to push")
 
 	return result
 }
